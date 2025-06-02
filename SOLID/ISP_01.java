27dce76e81d145cb49a2410e99fe11d2a89ab5c7
bhavika <bhavikaiyer12@gmail.com>
2025-06-02 20:47:01 +0200

    updated ISP_01

--- SOLID/ISP_01.java
+++ SOLID/ISP_01.java
@@ -1,6 +1,5 @@
 package SOLID;
 
-// 1. Break the big interface into smaller, segregated interfaces
 public class ISP_01 {
 
     public interface Printable {
@@ -15,7 +14,6 @@ public class ISP_01 {
         void faxDocument();
     }
 
-    // 2. SimplePrinter implements only what it supports
     public static class SimplePrinter implements Printable {
         @Override
         public void printDocument() {
